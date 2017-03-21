98528b48a16099895520b1fdbb8c4c1c158c594f
Scott McNally <scott.mcnally@pexmain.com>
2017-03-21 17:54:40 -0500

    Adding unit test project and reduced size test file

---
+++ src/PDFSharper.UnitTests/CompressionTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+
+namespace PDFSharper.UnitTests
+{
+    [TestClass]
+    public class CompressionTests
+    {
+        [TestMethod]
+        public void SaveReducedSize_NoModifications()
+        {
+            byte[] reducedPdf = GetReducedSizePdfData();
+        }
+
+        private static byte[] GetReducedSizePdfData()
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            using (var pdfStream = executingAssembly.GetManifestResourceStream("PDFSharper.UnitTests.ReducedSizePdf.pdf"))
+            {
+
+                byte[] result = new byte[pdfStream.Length];
+                pdfStream.Read(result, 0, result.Length);
+                return result;
+            }            
+        }
+    }
+}
---
+++ src/PDFSharper.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PDFSharper.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PDFSharper.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0070936b-2f45-40dd-a39d-8301ed8e7659")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
